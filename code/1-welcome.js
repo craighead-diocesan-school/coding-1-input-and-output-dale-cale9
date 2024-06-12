d29f1dc9ce19c4a27430a9aebab01bc977000197
dale-cale9 <145309461+dale-cale9@users.noreply.github.com>
2024-06-12 14:36:01 +1200

    no
    no

--- code/1-welcome.js
+++ code/1-welcome.js
@@ -3,17 +3,29 @@
 // ####################################
 
 function welcome() {
-  let name = 'Mary'
+  let name = prompt ('what is your name')
 
   alert('We want to know if you like programming!')
 
-  alert('Do you like programming ' + name + '?')
-  let answer = prompt()
+ 
+  let answer = prompt('Do you like programming ' + name + '?')
 
   alert('Great, you said ' + answer + '!')
   alert("Let's learn some code today.")
 }
 
 function questionnaire() {
-  // write the code for your questionnaire here
+  let name = prompt ('what is your name?')
+  alert('Hello ' + name )
+
+  let food = prompt('What did you have for breakfast ' + name + '?')
+alert('Amazing, I love ' + food + ' for breakfast')
+
+let colour = prompt ('what is your favourite colour ' + name + '?')
+alert('wonderful, I like ' + colour +  ' too!' )
+
+let age = prompt('How old are are you ?' )
+age = Number (age)
+age = age + 1
+alert ('you will be ' + age + ' next year!')
 }
