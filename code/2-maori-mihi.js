37191989a96e55b5deec6621b1f52790cecfa052
dale-cale9 <145309461+dale-cale9@users.noreply.github.com>
2024-06-12 15:16:16 +1200

    done
    done

--- code/2-maori-mihi.js
+++ code/2-maori-mihi.js
@@ -7,13 +7,17 @@ function maoriMihi() {
   alert('This program will help you build a basic mihi.')
   alert("First, you'll be asked a series of questions, then your mihi will be ready.")
 
-  alert('What is your name?')
-  let name = prompt()
-  // more questions and answers here
+
+  let name = prompt('What is your name?')
+  let mountain = prompt ('What is your mountain?')
+  let river = prompt ('What is your river?')
+  let town = prompt ('What is your home town?')
 
   alert('Your mihi:')
-  // more outputs here
-
+  alert('Tēnā koutou, katoa, ')
+  alert('ko ' + mountain + ' tōku maunga, ')
+  alert('ko ' + river + ' tōku awa, ')
+  alert('Nō ' + town + ' au, ')
   alert('Ko ' + name + ' tōku ingoa,')
   alert('Tēnā koutou,')
   alert('Tēnā koutou,')
--- code/3-non-maori-mihi.js
+++ code/3-non-maori-mihi.js
@@ -9,11 +9,20 @@ function nonMaoriMihi() {
 
   alert('What is your name?')
   let name = prompt()
-  // more questions and answers here
+  let mountain = prompt ('What mountain closest to your heart?')
+  let river = prompt ('What is your favourite river?')
+  let town = prompt ('What town did you grow up in?')
 
   alert('Your mihi:')
-  // more outputs here
+  
 
+  alert('Your mihi:')
+  alert('Tēnā koutou,')
+  alert('Kei te mihi au ki a ' + mountain)
+  alert('kei te mihi au ki a ' + river )
+  alert('Ko Te Tiriti o Waitangi te waka,')
+  alert('Ko Aotearoa te iwi,')
+  alert('Nō ' + town + ' au, ')
   alert('Ko ' + name + ' tōku ingoa,')
   alert('Tēnā koutou,')
   alert('Tēnā koutou,')
