1a149bd41db1884fce47b8f25272b3b02073caf5
dale-cale9 <145309461+dale-cale9@users.noreply.github.com>
2024-07-30 10:14:36 +1200

    c

--- code/1-welcome.js
+++ code/1-welcome.js
@@ -3,29 +3,29 @@
 // ####################################
 
 function welcome() {
-  let name = prompt ('what is your name')
+  let name = prompt('what is your name')
 
   alert('We want to know if you like programming!')
 
- 
+
   let answer = prompt('Do you like programming ' + name + '?')
 
   alert('Great, you said ' + answer + '!')
   alert("Let's learn some code today.")
 }
-
+const extrayear = 1
 function questionnaire() {
-  let name = prompt ('what is your name?')
-  alert('Hello ' + name )
+  let name = prompt('what is your name?')
+  alert('Hello ' + name)
 
   let food = prompt('What did you have for breakfast ' + name + '?')
-alert('Amazing, I love ' + food + ' for breakfast')
+  alert('Amazing, I love ' + food + ' for breakfast')
 
-let colour = prompt ('what is your favourite colour ' + name + '?')
-alert('wonderful, I like ' + colour +  ' too!' )
+  let colour = prompt('what is your favourite colour ' + name + '?')
+  alert('wonderful, I like ' + colour + ' too!')
 
-let age = prompt('How old are are you ?' )
-age = Number (age)
-age = age + 1
-alert ('you will be ' + age + ' next year!')
+  let age = prompt('How old are are you ?')
+  age = Number(age)
+  age = age + extrayear
+  alert('you will be ' + age + ' next year!')
 }
